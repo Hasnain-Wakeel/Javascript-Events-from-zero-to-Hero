d9baabbfcdb8e43da8035d4d8079dbcad6c8cdea
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2026-01-02 11:35:07 +0500

    setting field values Coded

---
+++ script.js
@@ -0,0 +1,39 @@
+function fillCampus() {
+  let rollNumber = Number(document.getElementById("rollNumber").value); 
+  // .value returns a string and our inputting value is in number
+  console.log("Roll Number =>" , rollNumber);
+
+  let campusValue;
+
+  switch (rollNumber) {
+    case 10:
+      campusValue = '"Zaitoon Ashraf IT Park"';
+      break;
+    case 20:
+      campusValue = '"Bahadurabad Campus"';
+      break;
+    case 30:
+      campusValue = '"Gulshan Campus"';
+      break;
+    case 40:
+      campusValue = '"Zamzama Campus"';
+      break;
+    case 50:
+      campusValue = '"Nazimabad Campus"';
+      break;
+    default:
+      alert("You are not Enrolled in any Campus!");
+      campusValue = "Not Enrolled in any Campus";
+
+  }
+
+  if(campusValue === "Not Enrolled in any Campus"){
+    document.getElementById("campus").value = ""
+    console.log("Campus =>" , campusValue);
+  }
+  else{
+    document.getElementById("campus").value = campusValue;
+    console.log("Campus =>" , campusValue);
+  }
+
+}
